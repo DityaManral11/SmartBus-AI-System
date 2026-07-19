dbe331e46b96dfe45ccb3148e75af935aaa04123
DityaManral11 <dityaguesswhy1102@gmail.com>
2026-07-19 22:49:17 +0530

    Configure frontend API for production deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_BASE_URL ||
+    "http://localhost:5000/api",
+
   headers: {
     "Content-Type": "application/json",
   },
@@ -17,9 +20,7 @@ api.interceptors.request.use(
 
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default api;
